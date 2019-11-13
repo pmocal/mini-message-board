ea6a4a3bf052394c6a3111abfe14b579b0e1b5b7
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-11-13 14:39:49 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const messages = [{text: "Hi there!", user: "Parthiv", added: new Date()}, {text: "Hello World!", user: "Charles", added: new Date()}];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;

36664316b7bd1082e90904a14f3563092682ea71
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-11-13 16:06:02 -0500

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const messages = [{text: "Hi there!", user: "Parthiv", added: new Date()}, {text: "Hello World!", user: "Charles", added: new Date()}];
+const messages = [{text: "Hi there!", user: "Parthiv", added: new Date()},
+	{text: "Hello World!", user: "Charles", added: new Date()}];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: "Mini Messageboard", messages: messages })
 });
 
 module.exports = router;
